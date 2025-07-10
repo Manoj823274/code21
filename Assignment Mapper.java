99e8f0754f3fd618c902f67bda4fad891b207b8d
Manoj823274 <basanimanoj65@gmail.com>
2025-07-10 10:10:49 +0530

    Create Assignment Mapper.java

---
+++ Assignment Mapper.java
@@ -0,0 +1,10 @@
+package com.example.school.mapper;
+
+import com.example.school.dto.*;
+import com.example.school.entity.*;
+import org.mapstruct.*;
+
+@Mapper(componentModel = "spring")
+public interface AssignmentMapper {
+    // Not used for now – placeholder
+}
